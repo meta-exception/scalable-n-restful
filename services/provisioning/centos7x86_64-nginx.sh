c1b5f0788cc2dea7b33089c957e311e3efe38acd
James Bruno <43601253+meta-exception@users.noreply.github.com>
2019-01-18 09:38:16 +0300

    services: add nginx-reverse-proxy

---
+++ services/provisioning/centos7x86_64-nginx.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -e
+
+# Update package list and upgrade all packages
+sudo yum -y update
+
+# Add
+sudo rpm -Uvh https://nginx.org/packages/mainline/centos/7/x86_64/RPMS/nginx-1.15.8-1.el7_4.ngx.x86_64.rpm
+
+# Install
+sudo yum -y install nginx
+
+# https://stackoverflow.com/questions/49597942/load-balancer-on-nginx-give-502-bad-gateway
+setsebool -P httpd_can_network_connect 1
+
+sudo echo -e "
+user  nginx;
+worker_processes  1;
+
+error_log  /var/log/nginx/error.log warn;
+pid        /var/run/nginx.pid;
+
+events {
+    worker_connections  1024;
+}
+
+http {
+    upstream backend_hosts {
+        server 10.1.0.2:8080;
+        server 10.1.0.3:8080;
+    }
+
+    server {
+        listen 8080;
+
+        location / {
+            proxy_pass http://backend_hosts;
+        }
+    }
+}
+" > /etc/nginx/nginx.conf
+
+sudo systemctl enable nginx
+
+sudo systemctl restart nginx
+#        server 10.1.0.4;
