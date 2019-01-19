671c0e012ca5e8a28018df55158a09ae6ffd7233
James Bruno <43601253+meta-exception@users.noreply.github.com>
2019-01-19 17:56:10 +0300

    app: remove duplicate style import

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import './plugins/vuetify';
 import App from './App.vue';
-import 'roboto-fontface/css/roboto/roboto-fontface.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import store from './store';
 
 Vue.config.productionTip = false;
